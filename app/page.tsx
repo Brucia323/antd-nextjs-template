0462a8147887c72c4120d5d2557e3a305993a399
周晨阳 <zhouchenyang@infuturespace.com>
2023-06-20 14:27:44 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,192 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import {
+  BellOutlined,
+  FolderOutlined,
+  QuestionCircleOutlined,
+} from '@ant-design/icons';
+import {
+  Avatar,
+  Breadcrumb,
+  Button,
+  Card,
+  Col,
+  Layout,
+  Menu,
+  Row,
+  Space,
+  Table,
+  Typography,
+  theme,
+} from 'antd';
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  position: string;
+  jurisdiction: string;
+}
+
+const data: DataType[] = [];
+
+for (let index = 0; index < 100; index++) {
+  data.push({
+    key: index,
+    name: index.toString(),
+    email: index.toString(),
+    phoneNumber: index.toString(),
+    position: index.toString(),
+    jurisdiction: index.toString(),
+  });
+}
 
 export default function Home() {
+  const {
+    token: { colorBgLayout, paddingMD, fontSizeXL, colorFill, padding },
+  } = theme.useToken();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <Layout style={{ minHeight: '100vh' }}>
+      <Layout.Header style={{ backgroundColor: colorBgLayout }}>
+        <Row justify="space-between">
+          <Col style={{ margin: 'auto 0' }}>
+            <div
+              style={{
+                width: 120,
+                height: 32,
+                backgroundColor: colorFill,
+              }}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          </Col>
+          <Col>
+            <Space size="large">
+              <BellOutlined />
+              <QuestionCircleOutlined />
+              <Avatar />
+            </Space>
+          </Col>
+        </Row>
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider style={{ backgroundColor: colorBgLayout }}>
+          <Menu
+            defaultSelectedKeys={['User management']}
+            defaultOpenKeys={['Dashboard']}
+            items={[
+              {
+                icon: <FolderOutlined />,
+                key: 'Dashboard',
+                label: 'Dashboard',
+                title: 'Dashboard',
+                children: [
+                  {
+                    key: 'Form',
+                    label: 'Form',
+                    title: 'Form',
+                  },
+                  {
+                    key: 'List',
+                    label: 'List',
+                    title: 'List',
+                  },
+                  {
+                    key: 'User management',
+                    label: 'User management',
+                    title: 'User management',
+                  },
+                  {
+                    key: 'Result',
+                    label: 'Result',
+                    title: 'Result',
+                  },
+                ],
+              },
+              {
+                icon: <FolderOutlined />,
+                key: 'Exception',
+                label: 'Exception',
+                title: 'Exception',
+                children: [],
+              },
+              {
+                icon: <FolderOutlined />,
+                key: 'Account',
+                label: 'Account',
+                title: 'Account',
+                children: [],
+              },
+              {
+                icon: <FolderOutlined />,
+                key: 'Editor',
+                label: 'Editor',
+                title: 'Editor',
+                children: [],
+              },
+            ]}
+            mode="inline"
+            style={{
+              backgroundColor: colorBgLayout,
+              height: '100%',
+              position: 'sticky',
+              top: 0,
+            }}
+          />
+        </Layout.Sider>
+        <Layout>
+          <Layout.Content style={{ padding: paddingMD }}>
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <Breadcrumb
+                items={[
+                  { title: 'Ant Design' },
+                  { title: 'Dashboard' },
+                  { title: 'user management' },
+                ]}
+              />
+              <Row justify="space-between">
+                <Col>
+                  <Typography.Text style={{ fontSize: fontSizeXL }}>
+                    user management
+                  </Typography.Text>
+                </Col>
+                <Col>
+                  <Space>
+                    <Button>Default Button</Button>
+                    <Button type="primary">Primary Button</Button>
+                  </Space>
+                </Col>
+              </Row>
+              <Card bodyStyle={{ padding: 0 }}>
+                <Table
+                  columns={[
+                    { dataIndex: 'name', title: 'Name' },
+                    { dataIndex: 'email', title: 'E-mail' },
+                    { dataIndex: 'phoneNumber', title: 'Phone number' },
+                    { dataIndex: 'position', title: 'Position' },
+                    { dataIndex: 'jurisdiction', title: 'Jurisdiction' },
+                    {
+                      key: 'operation',
+                      title: 'Operation',
+                      render: () => (
+                        <Space>
+                          <Button type="link">edit</Button>
+                          <Button type="link">drop out</Button>
+                        </Space>
+                      ),
+                    },
+                  ]}
+                  dataSource={data}
+                  pagination={{
+                    showSizeChanger: false,
+                    style: { paddingRight: padding },
+                  }}
+                />
+              </Card>
+            </Space>
+          </Layout.Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
 }
